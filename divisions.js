651a2a23ef0c536337d687d328c9f3c19c37a818
AliyaZamir <69330907+AliyaZamir@users.noreply.github.com>
2021-02-10 17:20:39 -0500

    Update divisions.js

--- divisions.js
+++ divisions.js
@@ -4,6 +4,17 @@ class Divisions{
         var options = {
             isStatic: true 
         }
-        this.body = Bodies.rectangles
+        this.body = Bodies.rectangle(x,y,w,h, options);
+        this.w = w;
+        this.h = h;
+        World.add(world, this.body);
+
     }
-}
+
+    display(){
+        var pos = this.body.position;
+        rectMode(CENTER);
+        fill("white");
+        rect(pos.x, pos.y, this.w, this.h);
+    }
+};
